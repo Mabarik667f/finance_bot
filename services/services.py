e623e08095d2516706fe75d2395db9582ecdd9f0
Mabarik667f <111702128+Mabarik667f@users.noreply.github.com>
2023-10-01 17:43:29 +0300

    fix date

--- services/services.py
+++ services/services.py
@@ -5,20 +5,23 @@ from aiogram.types import CallbackQuery, Message
 from lexicon.lexicon import LEXICON
 from models.methods import get_finance_request
 
+# дата сегодняшнего дня
 default_date = datetime.today().strftime('%Y-%m-%d')
 
 
 def get_date(days: int) -> str:
+    """Высчитывает до какого момента времени собирать данные по дате"""
     df_date = datetime.strptime(default_date, '%Y-%m-%d')
-    end_d = df_date + timedelta(days=days)
-    end_date_formatted = end_d.strftime('%Y-%m-%d')
+    start_d = df_date - timedelta(days=days)
+    start_date_formatted = start_d.strftime('%Y-%m-%d')
 
-    return end_date_formatted
+    return start_date_formatted
 
 
 def get_stat_data(request: (CallbackQuery, Message),
                   start_date=default_date,
                   end_date=default_date) -> None:
+    """Формирует данные за определённый период для вывода сообщения"""
     total_expense, total_income = get_finance_request(request.from_user.id,
                                                       start_date=start_date,
                                                       end_date=end_date)
